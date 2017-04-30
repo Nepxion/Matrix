1c88a6a36935d8f02044fba89cd73c89c41f78cd
Nepxion <1394997@qq.com>
2017-04-30 23:51:50 +0800

    增加AutoScanProxyDelegate

---
+++ src/main/java/com/nepxion/matrix/aop/AutoScanProxyDelegate.java
@@ -0,0 +1,77 @@
+package com.nepxion.matrix.aop;
+
+/**
+ * <p>Title: Nepxion Matrix</p>
+ * <p>Description: Nepxion Matrix AOP</p>
+ * <p>Copyright: Copyright (c) 2017</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @email 1394997@qq.com
+ * @version 1.0
+ */
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
+import org.aopalliance.intercept.MethodInterceptor;
+
+import com.nepxion.matrix.mode.ProxyMode;
+import com.nepxion.matrix.mode.ScanMode;
+
+public class AutoScanProxyDelegate extends AbstractAutoScanProxy {
+    private static final long serialVersionUID = 9073263289068871774L;
+
+    public AutoScanProxyDelegate() {
+        super();
+    }
+
+    public AutoScanProxyDelegate(String[] scanPackages) {
+        super(scanPackages);
+    }
+
+    public AutoScanProxyDelegate(ProxyMode proxyMode, ScanMode scanMode) {
+        super(proxyMode, scanMode);
+    }
+
+    public AutoScanProxyDelegate(String[] scanPackages, ProxyMode proxyMode, ScanMode scanMode) {
+        super(scanPackages, proxyMode, scanMode);
+    }
+
+    public AutoScanProxyDelegate(ProxyMode proxyMode, ScanMode scanMode, boolean exposeProxy) {
+        super(proxyMode, scanMode, exposeProxy);
+    }
+
+    public AutoScanProxyDelegate(String[] scanPackages, ProxyMode proxyMode, ScanMode scanMode, boolean exposeProxy) {
+        super(scanPackages, proxyMode, scanMode, exposeProxy);
+    }
+
+    @Override
+    protected Class<? extends MethodInterceptor>[] getCommonInterceptors() {
+        return null;
+    }
+
+    @Override
+    protected Object[] getAdditionalInterceptors(Class<?> targetClass) {
+        return null;
+    }
+
+    @Override
+    protected Class<? extends Annotation>[] getClassAnnotations() {
+        return null;
+    }
+
+    @Override
+    protected Class<? extends Annotation>[] getMethodAnnotations() {
+        return null;
+    }
+
+    @Override
+    protected void classAnnotationScanned(Class<?> targetClass, Class<? extends Annotation> classAnnotation) {
+
+    }
+
+    @Override
+    protected void methodAnnotationScanned(Class<?> targetClass, Method method, Class<? extends Annotation> methodAnnotation) {
+
+    }
+}
