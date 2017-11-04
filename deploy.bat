ebc1a7f039a3f07fb358a48d16d244eb01f83877
Nepxion <1394997@qq.com>
2017-11-04 10:25:35 +0800

    重构Maven结构，增加Test模块

--- deploy.bat
+++
@@ -1,5 +0,0 @@
-call mvn clean install -DskipTests
-call mvn install:install-file -DgroupId=nepxion -DartifactId=matrix -Dversion=1.0.0 -Dfile=target/matrix-1.0.0.jar -Dpackaging=jar -DgeneratePom=true
-
-pause
-
