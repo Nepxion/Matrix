081b0df0742287a2a38160131a92c3b113850487
Nepxion <1394997@qq.com>
2017-11-13 13:28:32 +0800

    更新注解

--- matrix-aop-test/src/main/java/com/nepxion/matrix/test/simple/aop/MyAnnotation1.java
+++ matrix-aop-test/src/main/java/com/nepxion/matrix/test/simple/aop/MyAnnotation1.java
@@ -10,15 +10,17 @@ package com.nepxion.matrix.test.simple.aop;
  * @version 1.0
  */
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Inherited()
 @Target({ ElementType.METHOD, ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
 public @interface MyAnnotation1 {
     String name();
 
