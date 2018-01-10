c6db16090a3f61fac84216836731b3064d02f3b8
Nepxion <1394997@qq.com>
2018-01-11 01:03:15 +0800

    重构类结构

--- matrix-aop-spring-boot-example/src/main/java/com/nepxion/matrix/complex/service/MyService5Impl.java
+++ matrix-aop-spring-boot-example/src/main/java/com/nepxion/matrix/complex/service/MyService5Impl.java
@@ -3,7 +3,7 @@ package com.nepxion.matrix.complex.service;
 /**
  * <p>Title: Nepxion Matrix</p>
  * <p>Description: Nepxion Matrix AOP</p>
- * <p>Copyright: Copyright (c) 2017</p>
+ * <p>Copyright: Copyright (c) 2017-2020</p>
  * <p>Company: Nepxion</p>
  * @author Haojun Ren
  * @email 1394997@qq.com
