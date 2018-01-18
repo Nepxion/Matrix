fa1eb648e2ad934202ce192390bd0bc2c95e4012
Nepxion <1394997@qq.com>
2018-01-18 15:56:12 +0800

    修改脚本

--- version.bat
+++ version.bat
@@ -5,8 +5,8 @@
 @echo $                                                           $
 @echo $                                                           $
 @echo $                                                           $
-@echo $  Nepxion Technologies All Right Reserved                  $
-@echo $  Copyright(C) 2017                                        $
+@echo $  Nepxion Studio All Right Reserved                        $
+@echo $  Copyright (C) 2017-2050                                  $
 @echo $                                                           $
 @echo =============================================================
 @echo.
@@ -15,5 +15,6 @@
 @title Nepxion Matrix
 @color 0a
 
-mvn versions:set -DnewVersion=1.0.8
+call mvn versions:set -DnewVersion=1.0.8
+
 pause
