9863db8820b3ccec645990bc36858e162681ec0c
Nepxion <1394997@qq.com>
2018-06-27 17:09:23 +0800

    升级版本

---
+++ version.bat
@@ -0,0 +1,20 @@
+@echo on
+@echo =============================================================
+@echo $                                                           $
+@echo $                       Nepxion Matrix                      $
+@echo $                                                           $
+@echo $                                                           $
+@echo $                                                           $
+@echo $  Nepxion Studio All Right Reserved                        $
+@echo $  Copyright (C) 2017-2050                                  $
+@echo $                                                           $
+@echo =============================================================
+@echo.
+@echo off
+
+@title Nepxion Matrix
+@color 0a
+
+call mvn versions:set -DnewVersion=1.0.18
+
+pause
