b98b6268a528366fb2dcfd10c47efcefa63b9e98
Nepxion <1394997@qq.com>
2019-04-22 14:38:52 +0800

    升级版本到2.0.6

--- matrix-aop/src/main/java/com/nepxion/matrix/constant/MatrixConstant.java
+++ matrix-aop/src/main/java/com/nepxion/matrix/constant/MatrixConstant.java
@@ -10,5 +10,5 @@ package com.nepxion.matrix.constant;
  */
 
 public class MatrixConstant {
-    public static final String MATRIX_VERSION = "2.0.5";
+    public static final String MATRIX_VERSION = "2.0.6";
 }
